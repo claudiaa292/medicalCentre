a011c7d9a60dc3f6afb1c3393a7cde834f9dee95
Claudia <dogaruclaudia.roxana@gmail.com>
2024-07-18 23:04:37 +0300

    new branch

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const add = document.getElementById("add");
 const nume = document.getElementById("nume");
 const lista = document.querySelector("#lista");
 
-
-
 const addNewElem = function () {
+    
   const list = document.createElement("li");
-  const newText = document.createTextNode("Text adaugat din js");
+  const newText = document.createTextNode(nume.value);
+
   list.appendChild(newText);
   lista.appendChild(list);
+  nume.value = "";
 };
 
 add.addEventListener("click", addNewElem);
