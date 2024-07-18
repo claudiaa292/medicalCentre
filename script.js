0caaa0025767aaaae7356573efad8906052e73d9
Claudia <dogaruclaudia.roxana@gmail.com>
2024-07-18 22:33:43 +0300

    add element in list

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello Alin");
+
+const add = document.getElementById("add");
+const nume = document.getElementById("nume");
+const lista = document.querySelector("#lista");
+
+
+
+const addNewElem = function () {
+  const list = document.createElement("li");
+  const newText = document.createTextNode("Text adaugat din js");
+  list.appendChild(newText);
+  lista.appendChild(list);
+};
+
+add.addEventListener("click", addNewElem);
